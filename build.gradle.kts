a88b0c94f748090a16e636327daef78af1ead2b9
kei-1111 <kei_02_1111@icloud.com>
2024-12-11 16:23:38 +0900

    feat: 基本的なライブラリの追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,16 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+//    detekt
+    alias(libs.plugins.detekt) apply false
+
+//    KSP
+    alias(libs.plugins.ksp) apply false
+
+//    Hilt
+    alias(libs.plugins.hilt) apply false
+
+//    Serialization
+    alias(libs.plugins.serialization) apply false
 }
