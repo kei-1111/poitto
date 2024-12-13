ef3d084dae91a0d39ef52684c8e1e38ab5611ed1
kei-1111 <kei_02_1111@icloud.com>
2024-12-13 23:22:31 +0900

    feat: ユーザ情報保存タイミングの問題により、UIの変更を行った

--- app/src/main/java/com/example/flush/ui/feature/user_settings/UserSettingsScreen.kt
+++ app/src/main/java/com/example/flush/ui/feature/user_settings/UserSettingsScreen.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavDestination
 import com.example.flush.ui.compose.BodyMediumText
-import com.example.flush.ui.compose.BottomBar
 import com.example.flush.ui.compose.CenteredContainer
 import com.example.flush.ui.compose.FilledButton
 
@@ -16,16 +14,8 @@ import com.example.flush.ui.compose.FilledButton
 fun UserSettingsScreen(
     navigateToSearch: () -> Unit,
     navigateToAuthSelection: () -> Unit,
-    currentDestination: NavDestination?,
 ) {
-    Scaffold(
-        bottomBar = {
-            BottomBar(
-                navigateToSearch = navigateToSearch,
-                currentDestination = currentDestination,
-            )
-        },
-    ) { innerPadding ->
+    Scaffold { innerPadding ->
         CenteredContainer(
             modifier = Modifier.padding(innerPadding),
         ) {
