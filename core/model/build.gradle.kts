6a0e594c169784d767305605024c9f329d9d0d41
kei-1111 <kei_02_1111@icloud.com>
2025-03-19 22:17:41 +0900

    feat: core.modelモジュールにカスタムプラグインを適応し、不要なものを削除

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -1,54 +1,8 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-
-//    Serialization
-    alias(libs.plugins.serialization)
+    alias(libs.plugins.flush.android.library.compose)
+    alias(libs.plugins.flush.kotlin.serialization)
 }
 
 android {
     namespace = "com.example.flush.core.model"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
-}
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-
-//    Serialization
-    implementation(libs.kotlinx.serialization.json)
 }
