d4e1973d3f0206aed2dda5d806f3461bd981f64b
kei-1111 <kei_02_1111@icloud.com>
2025-03-19 22:33:52 +0900

    feat: data.repositoryモジュールにカスタムプラグインを適応し、不要なものを削除

--- data/repository/build.gradle.kts
+++ data/repository/build.gradle.kts
@@ -1,73 +1,26 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.flush.android.library)
 
-//    Google Services
     alias(libs.plugins.google.services)
-
-//    Secrets Gradle Plugin
     alias(libs.plugins.secrets.gradle.plugin)
 }
 
 android {
     namespace = "com.example.flush.data.repository"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-
-        buildConfigField("String", "WEB_CLIENT_ID", "\"${System.getenv("WEB_CLIENT_ID") ?: ""}\"")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        buildConfig = true
-    }
+    buildFeatures.buildConfig = true
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(projects.data.api)
-
     implementation(projects.core.model)
     implementation(projects.core.repository)
     implementation(projects.core.utils)
+    implementation(projects.data.api)
 
-//    Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
-
-//    Coroutines
-    implementation(libs.kotlinx.coroutines.play.services)
-
-//    Google Auth
-    implementation(libs.play.services.auth)
-
-//    kotlin-result
     implementation(libs.kotlin.result)
+    implementation(libs.kotlinx.coroutines.play.services)
+    implementation(libs.play.services.auth)
 }
