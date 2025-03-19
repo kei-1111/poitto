aa94121c7460a21af6be3717d463ba799d2499b5
kei-1111 <kei_02_1111@icloud.com>
2025-03-19 22:29:16 +0900

    feat: data.apiモジュールにカスタムプラグインを適応し、不要なものを削除

--- data/api/build.gradle.kts
+++ data/api/build.gradle.kts
@@ -1,52 +1,15 @@
-import com.google.crypto.tink.internal.Serialization
-
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-
-//    Serialization
-    alias(libs.plugins.serialization)
+    alias(libs.plugins.flush.android.library)
+    alias(libs.plugins.flush.kotlin.serialization)
 }
 
 android {
     namespace = "com.example.flush.data.api"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
     implementation(projects.core.model)
 
-    //    Retrofit
     implementation(libs.retrofit2)
     implementation(libs.retrofit2.kotlinx.serialization.converter)
 }
