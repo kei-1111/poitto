f27598d057423c17d647b471baa151db3e476ef0
kei-1111 <kei_02_1111@icloud.com>
2025-03-19 21:22:06 +0900

    feat: core.designsystemモジュールにカスタムプラグインを適応し、不要なものを削除

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,60 +1,14 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.flush.android.library.compose)
 }
 
 android {
     namespace = "com.example.flush.core.designsystem"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-//    Material Icon Extended
     implementation(libs.androidx.material.icons.extended)
-
-//    Lottie
-    implementation(libs.lottie.compose)
-
-//    Coil
+    implementation(libs.androidx.material3)
     implementation(libs.coil.compose)
+    implementation(libs.lottie.compose)
 }
