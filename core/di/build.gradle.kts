65d78f36ee9cab513802fcdc7837d6060223728d
kei-1111 <kei_02_1111@icloud.com>
2025-03-19 21:30:18 +0900

    feat: core.diモジュールにカスタムプラグインを適応し、不要なものを削除

--- core/di/build.gradle.kts
+++ core/di/build.gradle.kts
@@ -1,76 +1,24 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-
-//    KSP
-    alias(libs.plugins.ksp)
-
-//    Hilt
-    alias(libs.plugins.hilt)
+    alias(libs.plugins.flush.android.library)
+    alias(libs.plugins.flush.hilt)
 }
 
 android {
     namespace = "com.example.flush.core.di"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
     implementation(projects.data.api)
     implementation(projects.data.repository)
     implementation(projects.core.repository)
 
-
-//    Hilt
-    ksp(libs.hilt.android.compiler)
-    implementation(libs.hilt.android)
-    implementation(libs.hilt.navigation.compose)
-
-//    Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
-
-//    Google Auth
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.okhttp)
     implementation(libs.play.services.auth)
-
-//    Retrofit
     implementation(libs.retrofit2)
     implementation(libs.retrofit2.kotlinx.serialization.converter)
-
-//    OkHttp
-    implementation(libs.okhttp)
-
-//    Serialization
-    implementation(libs.kotlinx.serialization.json)
 }
